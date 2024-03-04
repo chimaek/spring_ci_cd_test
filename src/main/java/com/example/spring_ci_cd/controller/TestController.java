2bbd5e2b39e1b20f3c9474f9d26b5ce6de6acf28
seungju.kim <pipiru100@gmail.com>
2024-03-04 12:18:35 +0900

    add

--- src/main/java/com/example/spring_ci_cd/controller/TestController.java
+++ src/main/java/com/example/spring_ci_cd/controller/TestController.java
@@ -3,16 +3,19 @@ package com.example.spring_ci_cd.controller;
 import com.example.spring_ci_cd.service.TestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class TestController {
 
   private final TestService testService;
 
-  @GetMapping("/test")
+  @GetMapping("/test") // localhost:8080/api/test
   public String getTestString() {
+
     return testService.getTestString();
   }
 }
